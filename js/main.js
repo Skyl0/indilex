6b14a620582447d448aa357bd97cd2fe36a6de7f
Marc Ernst <ernst.marc@gmail.com>
2015-04-28 13:38:13 +0200

    fixes mit tobi

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ jQuery(document).ready(function($)  {
 		      if (target.length) {
 		        $('html,body').animate({
 		          scrollTop: target.offset().top
-		        }, 1000);
+		        }, 800);
 		        return false;
 	      	  }
 	    	}
